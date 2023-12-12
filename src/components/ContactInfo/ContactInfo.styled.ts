db8bde832a1d9b2c2c296120ca14c6260e75687a
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2023-12-12 14:34:35 +0200

    Update ContactInfo.styled.ts

--- src/components/ContactInfo/ContactInfo.styled.ts
+++ src/components/ContactInfo/ContactInfo.styled.ts
@@ -1,28 +1,30 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  /* display: flex; */
-  /* flex-direction: column; */
-  /* gap: ${({ theme }) => theme.spacing(10)}; */
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing(10)};
 `;
 
 export const Field = styled.div`
-  /* display: flex; */
-  /* justify-content: space-between; */
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const Info = styled.div``;
 
 export const Title = styled.p`
-  /* color: ${({ theme }) => theme.colors.secondaryFontColor}; */
-  /* font-family: Inter; */
-  /* font-size: ${({ theme }) => theme.fontSize.primaryFontSize}px; */
-  /* font-weight: ${({ theme }) => theme.fontWeight.primaryFontWeight}; */
+  color: ${({ theme }) => theme.colors.secondaryFontColor};
+  font-family: Inter;
+  font-size: ${({ theme }) => theme.fontSize.primaryFontSize}px;
+  font-weight: ${({ theme }) => theme.fontWeight.primaryFontWeight};
 `;
 
 export const Description = styled.p`
-  /* color: ${({ theme }) => theme.colors.primaryFontColor}; */
-  /* font-family: Inter; */
-  /* font-size: ${({ theme }) => theme.fontSize.primaryFontSize}px; */
-  /* font-weight: ${({ theme }) => theme.fontWeight.primaryFontWeight}; */
+  width: 175px;
+  color: ${({ theme }) => theme.colors.primaryFontColor};
+  font-family: Inter;
+  font-size: ${({ theme }) => theme.fontSize.primaryFontSize}px;
+  font-weight: ${({ theme }) => theme.fontWeight.primaryFontWeight};
+  ${({ theme }) => theme.trimText}
 `;
