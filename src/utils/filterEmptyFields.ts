1eef5de23c0fa4c6b488ed566e0c357dfb8d4443
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2023-12-15 20:19:11 +0200

    Update filterEmptyFields.ts

--- src/utils/filterEmptyFields.ts
+++ src/utils/filterEmptyFields.ts
@@ -2,11 +2,9 @@ import { ProfileEntry } from 'types/types';
 
 const filterEmptyFields = <T>(data: T): T => {
   const entries: ProfileEntry[] = Object.entries(data as Object);
-  const filteredEntries = entries.map((entry) =>
-    entry[1] === '' ? [entry[0], undefined] : entry
-  );
+  const filteredEntries = entries.filter((entry) => entry[1]);
 
-  return Object.fromEntries(filteredEntries);
+  return Object.fromEntries(filteredEntries) as T;
 };
 
 export default filterEmptyFields;
